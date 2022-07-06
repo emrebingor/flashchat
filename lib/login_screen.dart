9002c5afd63a61e54e8a115793c6ae0493c3ed13
emrebingor <emrebingor5@gmail.com>
2022-07-06 19:09:37 +0300

    Textfield design

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,7 +1,7 @@
+import 'package:flashchat_app_flutter/constant.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-
   static const String id = 'login_screen';
 
   @override
@@ -11,6 +11,34 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: Hero(
+              tag: 'logo',
+              child: Image(
+                image: AssetImage('images/logo.png'),
+              ),
+            ),
+          ),
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            textAlign: TextAlign.center,
+            onChanged: (value) {},
+            decoration: kTextFieldStyle.copyWith(hintText: 'Enter your mail'),
+          ),
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            textAlign: TextAlign.center,
+            onChanged: (value){
+
+            },
+            decoration: kTextFieldStyle.copyWith(hintText: 'Enter your password'),
+          ),
+        ],
+      ),
+    );
   }
 }
