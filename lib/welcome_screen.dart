045769261f09d97ad26e7fd760a0fd82a824087a
emrebingor <emrebingor5@gmail.com>
2022-07-06 18:53:14 +0300

    Animation addition

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -5,20 +5,38 @@ import 'package:flutter/animation.dart';
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
 
+
   @override
   State<WelcomeScreen> createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
 
-  AnimationController controller;
-  Animation animation;
+  late AnimationController controller;
+  late Animation animation;
+
+
+  @override
+  void initState(){
+    super.initState();
+
+    controller = AnimationController(vsync: this, duration: Duration(seconds: 1),);
+
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white).animate(controller);
+
+    controller.forward();
+
+    controller.addListener(() {
+      setState((){});
+    });
+  }
+
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
