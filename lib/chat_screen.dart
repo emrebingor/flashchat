25c135c16e6ee523be31ba852719ba9ee38e9a4f
emrebingor <emrebingor5@gmail.com>
2022-07-09 13:47:35 +0300

    Message text edits and list view addition

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChatScreen extends StatefulWidget {
+final _firestore = FirebaseFirestore.instance;
+final _auth = FirebaseAuth.instance;
 
+class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
 
   @override
@@ -13,41 +15,34 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
-  final _auth = FirebaseAuth.instance;
-  final _firestore = FirebaseFirestore.instance;
+  final messageTextController = TextEditingController();
   User? loggedInUser;
   String? message;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     getCurrentUser();
   }
 
-
   void getCurrentUser() async {
-
     try {
       final user = await _auth.currentUser;
       if (user != null) {
         loggedInUser = user;
       }
-      } catch (e){
-        print(e);
+    } catch (e) {
+      print(e);
     }
+  }
+
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
     }
-
-
-
-    void messagesStream() async{
-     await for(var snapshot in _firestore.collection('messages').snapshots()){
-       for(var message in snapshot.docs){
-         print(message.data());
-       }
-     }
-    }
-
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,35 +67,7 @@ class _ChatScreenState extends State<ChatScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          StreamBuilder<QuerySnapshot>(
-            stream: _firestore.collection('messages').snapshots(),
-            builder: (context, snapshot){
-              List<Text> messageWidgets = [];
-              if (!snapshot.hasData){
-                return Center(
-                  child: CircularProgressIndicator(
-                    backgroundColor: Colors.lightBlueAccent,
-                  ),
-                );
-              }
-                final messages = snapshot.data?.docs;
-
-                for (var message in messages!){
-                  try{
-
-                    final messageText = message.get('text');
-                    final messageSender = message.get('sender');
-                    final messageWidget = Text('$messageText from $messageSender');
-
-                    messageWidgets.add(messageWidget);
-                  } catch(e){
-                    print(e);
-                    }
-                  }
-                return Column(children: messageWidgets,
-                );
-            },
-          ),
+          MessagesStream(),
           Container(
             decoration: kContainerDecoration,
             child: Row(
@@ -108,6 +75,7 @@ class _ChatScreenState extends State<ChatScreen> {
               children: <Widget>[
                 Expanded(
                   child: TextField(
+                    controller: messageTextController,
                     onChanged: (value) {
                       message = value;
                     },
@@ -116,6 +84,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 ),
                 TextButton(
                   onPressed: () {
+                    messageTextController.clear();
                     _firestore.collection('messages').add({
                       'text': message,
                       'sender': loggedInUser?.email,
@@ -127,6 +96,89 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                 ),
               ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class MessagesStream extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('messages').snapshots(),
+      builder: (context, snapshot) {
+        List<MessageBubble> messageBubbles = [];
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+        final messages = snapshot.data?.docs;
+
+        for (var message in messages!) {
+          try {
+            final messageText = message.get('text');
+            final messageSender = message.get('sender');
+            final messageBubble = MessageBubble(
+              sender: messageSender,
+              text: messageText,
+            );
+
+            messageBubbles.add(messageBubble);
+          } catch (e) {
+            print(e);
+          }
+        }
+        return Expanded(
+          child: ListView(
+            padding:
+            EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children: messageBubbles,
+          ),
+        );
+      },
+    );
+  }
+}
+
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble({this.sender, this.text});
+
+  final String? sender;
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text(
+            '$sender',
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.black54,
+            ),
+          ),
+          Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(30.0),
+            color: Colors.lightBlueAccent,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                '$text',
+                style: TextStyle(fontSize: 15.0, color: Colors.white),
+              ),
             ),
           ),
         ],
